787a05bac3509b7f4db310d2fd8821144dc7f165
dsoem <Soemo>
2020-01-21 10:54:22 +0100

    JavaScript

---
+++ h02/index2.1.js
@@ -0,0 +1,4 @@
+document.getElementById("paginatitel").innerText = "Mijn geweldige pagina";
+document.getElementById("subknop").innerText = "Mijn geweldige subkop";
+document.getElementById("Samsung").src = "img/SamsungS10.jpg";
+document.getElementById("Iphone").src = "img/iphoneX.jpg";
---
+++ h02/index2.2.js
@@ -0,0 +1,26 @@
+var a=document.getElementById("pink");
+a.style.backgroundColor="pink";
+a.style.position="absolute";
+a.style.left="0px";
+a.style.top="0px";
+
+var a=document.getElementById("orange");
+a.style.backgroundColor="orange";
+a.style.position="absolute";
+a.style.left="100px";
+a.style.top="20px";
+
+
+var a=document.getElementById("white");
+a.style.backgroundColor="white";
+a.style.position="absolute";
+a.style.left="200px";
+a.style.top="60px";
+
+var a=document.getElementById("purple");
+a.style.backgroundColor="purple";
+a.style.position="absolute";
+a.style.left="300px";
+a.style.top="0px";
+
+
